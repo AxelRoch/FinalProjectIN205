d876064d493db90ab7cd35f91bb96423d6f969ea
alizeebasset <abasset@LAPTOP-TD2TKF7M.localdomain>
2020-03-03 11:48:21 +0100

    structure début exo 2

---
+++ Projet_Final/LibraryManager/src/com/excilys/librarymanager/dao/LivreDao.java
@@ -0,0 +1,13 @@
+import java.util.List;
+
+import com.excilys.librarymanager.exception.DaoException;
+import com.excilys.librarymanager.modele.Livre;
+
+public interface LivreDao {
+	public List<Livre> getList() throws DaoException;
+	public Livre getById(int id) throws DaoException;
+	public int create(String titre, String auteur, String isbn) throws DaoException;
+	public void update(Livre livre) throws DaoException;
+	public void delete(int id) throws DaoException;
+	public int count() throws DaoException;
+}
