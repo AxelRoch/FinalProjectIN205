da833f1f99c3b432d8024bca2ebcd137166456a5
alizeebasset <48725874+alizeebasset@users.noreply.github.com>
2020-03-20 18:31:48 +0100

    Début EmpruntDaoImpl.java
    squelette et commandes sql

--- Projet_Final/LibraryManager/src/com/excilys/librarymanager/dao/impl/EmpruntDaoImpl.java
+++ Projet_Final/LibraryManager/src/com/excilys/librarymanager/dao/impl/EmpruntDaoImpl.java
@@ -1,19 +1,32 @@
 package com.excilys.librarymanager.dao.impl;
+import java.util.List;
 
 import com.excilys.librarymanager.exception.DaoException;
 import com.excilys.librarymanager.modele.Emprunt;
 import com.excilys.librarymanager.modele.EmpruntDao;
+import com.excilys.librarymanager.modele.Abonnement;
 import com.excilys.librarymanager.persistence.ConnectionManager;
 
+import java.sql.ResultSet;
 import java.sql.Statement;
 
-public class EmpruntDaoImpl implements EmpruntDaoImpl
+public class EmpruntDaoImpl implements EmpruntDao
 {
+	private static EmpruntDaoImpl instance;
+	private EmpruntDaoImpl() { }	
+	public static EmpruntDaoImpl getInstance() {
+		if(instance == null) {
+			instance = new EmpruntDaoImpl();
+		}
+		return instance;
+	}
 
-
-
-
-    private static final String SELECT_ALL_QUERY = "SELECT * FROM Emprunt;";
+    private static final String SELECT_ALL_QUERY = "SELECT id, membre, livre, dateEmprunt, dateRetour;");
+    private static final String SELECT_ONE_QUERY = "SELECT id, membre, livre, dateEmprunt, dateRetour FROM membre WHERE id = ?;";
+    private static final String CREATE_QUERY =  "INSERT INTO emprunt(membre, livre, dateEmprunt, dateRetour) VALUES (?, ?, ?, ?);";
+    private static final String UPDATE_QUERY = "UPDATE emprunt SET membre = ?, livre = ?, dateEmprunt = ?, dateRetour = ? WHERE id = ?;";
+    private static final String DELETE_QUERY = "DELETE FROM emprunt WHERE id = ?;";
+    private static final String COUNT_QUERY = "SELECT COUNT(id) AS count FROM emprunt;";
 
 
     @Override
@@ -27,7 +40,7 @@ public class EmpruntDaoImpl implements EmpruntDaoImpl
             )
             {
 			while(res.next()) {
-				Emprunt e = new Emprunt(res.getInt("id"), res.getString("titre"), res.getString("realisateur"));
+				Emprunt e = new Emprunt(res.getInt("id"), res.getString("titre"), res.getString("realisateur"));   ///à changer
 				emprunts.add(e);
 			}
 			System.out.println("GET: " + emprunts);
@@ -36,3 +49,29 @@ public class EmpruntDaoImpl implements EmpruntDaoImpl
 		}
 		return emprunts;
 	}
+	
+	public List<Emprunt> getList() throws DaoException{
+	}
+	
+	public List<Emprunt> getListCurrent() throws DaoException{
+	}
+	
+	public List<Emprunt> getListCurrentByMembre(int idMembre) throws DaoException{
+	}
+	
+	public List<Emprunt> getListCurrentByLivre(int idLivre) throws DaoException{
+	}
+	
+	public Emprunt getById(int id) throws DaoException{
+	}
+	
+	public void create(int idMembre, int idLivre, LocalDate dateEmprunt) throws DaoException{
+	}
+	
+	public void update(Emprunt emprunt) throws DaoException{
+	}
+	
+	public int count() throws DaoException{
+	}
+	
+}
