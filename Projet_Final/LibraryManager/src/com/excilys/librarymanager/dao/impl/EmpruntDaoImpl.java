e2829f4894a959641477c23667dacbf412255592
administrateur <administrateur@MacBook-Air-de-administrateur.local>
2020-03-21 10:53:01 +0100

    correction

--- Projet_Final/LibraryManager/src/com/excilys/librarymanager/dao/impl/EmpruntDaoImpl.java
+++ Projet_Final/LibraryManager/src/com/excilys/librarymanager/dao/impl/EmpruntDaoImpl.java
@@ -2,18 +2,23 @@ package com.excilys.librarymanager.dao.impl;
 
 import com.excilys.librarymanager.exception.DaoException;
 import com.excilys.librarymanager.modele.Emprunt;
-import com.excilys.librarymanager.modele.EmpruntDao;
+import com.excilys.librarymanager.dao.EmpruntDao;
 import com.excilys.librarymanager.persistence.ConnectionManager;
 
 import java.sql.Statement;
 
-public class EmpruntDaoImpl implements EmpruntDaoImpl
+public class EmpruntDaoImpl implements EmpruntDao
 {
 
+    private static EmpruntDaoImpl instance;
+	private EmpruntDaoImpl() { }	
+	public static EmpruntDaoImpl getInstance() {
+		if(instance == null) {
+			instance = new EmpruntDaoImpl();
+		}
+		return instance;
+	}
 
-
-
-    private static final String SELECT_ALL_QUERY = "SELECT * FROM Emprunt;";
 
 
     @Override
@@ -21,18 +26,24 @@ public class EmpruntDaoImpl implements EmpruntDaoImpl
     {
         List<Emprunt> emprunts = new ArrayList<>();
 		
-		try (Connection connection = EstablishConnection.getConnection();
-			 PreparedStatement preparedStatement = connection.prepareStatement(SELECT_ALL_QUERY);
+    
+    @Override
+    public List<Emprunt> getListCurrent() throws DaoException
+    {
+        List<Emprunt> emprunts = new ArrayList<>();
+		
+		try (Connection connection = ConnectionManager.getConnection();
+			 PreparedStatement preparedStatement = connection.prepareStatement(SELECT_ALL_CURRENT_QUERY);
 			 ResultSet res = preparedStatement.executeQuery();
             )
             {
 			while(res.next()) {
 				Emprunt e = new Emprunt(res.getInt("id"), res.getString("titre"), res.getString("realisateur"));
 				emprunts.add(e);
-			}
+		}
 			System.out.println("GET: " + emprunts);
 		} catch (SQLException exc) {
 			throw new DaoException("Probleme lors de la recuperation de la liste des emprunts", exc);
 		}
-		return emprunts;
-	}
+        return emprunts;
+    }
